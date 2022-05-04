f5af21701516dcf788cffac0c41f725052ac74a0
Anant Vikram Singh <anantvikramsr.cs20@rvce.edu.in>
2022-05-04 18:46:30 +0530

    Initial commit

--- intro-code/make-copies.sh
+++
@@ -1,4 +0,0 @@
-i=-1
-while (( i++ < 30)); do
-  cp ./disk-files/foo.pdf "./disk-files/foo$i.pdf"
-done
--- v1.c
+++
@@ -1,9 +0,0 @@
-
-
-warri00r@Gideon:~/anant/intro-code$ ps aux
-USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND
-root        8194  0.0  0.0      0     0 ?        I    15:26   0:00 [kworker/3:1-events]
-root        8230  0.0  0.0      0     0 ?        I    15:29   0:00 [kworker/5:2]
-root        8235  0.0  0.0      0     0 ?        I    15:30   0:00 [kworker/7:0-mm_percpu_wq]
-warri00r    8259  0.0  0.0   6280  4932 pts/0    S+   15:31   0:00 ./a.out
-warri00r    8260  0.0  0.0  11708  3508 pts/1    R+   15:31   0:00 ps aux
